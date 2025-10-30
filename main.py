59208e9180e8f834161e52e38734aa851b5a8f92
wtrojak <wtrojak@student.agh.edu.pl>
2025-10-30 12:56:21 +0100

    zajecia

--- main.py
+++ main.py
@@ -8,9 +8,9 @@
 #     - Weryfikujące poprawność wyników dla prawidłowych danych wejściowych.
 # =============================================================================
 import numpy as np
+from typing import Union
 
-
-def spare_matrix_Abt(m: int, n: int) -> tuple[np.ndarray, np.ndarray] | None:
+def spare_matrix_Abt(m: int, n: int) -> Union[tuple[np.ndarray, np.ndarray], None]:
     """Funkcja tworząca zestaw składający się z macierzy A (m,n) i
     wektora b (m,) na podstawie pomocniczego wektora t (m,).
 
@@ -24,12 +24,19 @@ def spare_matrix_Abt(m: int, n: int) -> tuple[np.ndarray, np.ndarray] | None:
             - Wektor b (m,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    if not isinstance(n, int) or not isinstance(m, int) or n<=0 or m<=0:
+        return None
+
+    t = np.linspace(0,1,m)
+    A = np.vander(t,n,True)
+    b = np.array([np.cos(4*i) for i in t ])
+    return A , b
+
 
 
 def square_from_rectan(
     A: np.ndarray, b: np.ndarray
-) -> tuple[np.ndarray, np.ndarray] | None:
+) -> Union[tuple[np.ndarray, np.ndarray], None]:
     """Funkcja przekształcająca układ równań z prostokątną macierzą współczynników
     na kwadratowy układ równań.
     A^T * A * x = A^T * b  ->  A_new * x = b_new
@@ -44,10 +51,16 @@ def square_from_rectan(
             - Wektor b_new (n,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
+    if not isinstance(A , np.ndarray) or not isinstance(b, np.ndarray) or len(A)!= len(b) or np.ndim(b) != 1 or np.ndim(A)!=2:
+        return None 
+    
+    A_new = A.transpose()@A
+    b_new = A.transpose()@b
+
+    return A_new,b_new
 
 
-def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
+def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> Union[float, None]:
     """Funkcja obliczająca normę residuum dla równania postaci:
     Ax = b
 
